e3bdc7ed7fc292a7120d46bfe6f1f2bcd0609b16
Puvl <pa.re.14@mail.ru>
2020-12-10 19:47:44 +0300

    new version 0.2(Задача Капучино)

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
 from sys import argv, exit
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidget,\
-    QTableWidgetItem
+from PyQt5.QtWidgets import QApplication, QMainWindow,\
+    QTableWidgetItem, QMenu, QAction, QMessageBox
 from PyQt5 import uic
 import sqlite3
 
 
-class Cofeetable(QMainWindow):
+class Coffeetable(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('UI.ui', self)
         self.connection = sqlite3.connect('cofee.sqlite')
         self.cur = self.connection.cursor()
-        self.data_from_table = \
-            self.cur.execute('''SELECT * FROM data''').fetchall()
         self.initUI()
 
     def initUI(self):
+        self.show_table()
+
+        self.bar = self.menuBar()
+
+        self.action = QAction('Добавить запись', self)
+        self.action.triggered.connect(self.open_add_menu)
+
+        self.menu = QMenu()
+        self.menu.setTitle('Инструменты')
+        self.menu.addAction(self.action)
+        self.bar.addMenu(self.menu)
+
+    def show_table(self):
+        self.data_from_table = \
+            self.cur.execute('''SELECT * FROM data''').fetchall()
+
         self.tableWidget.setColumnCount(len(self.data_from_table[0]))
         self.tableWidget.setRowCount(len(self.data_from_table))
         self.tableWidget.setHorizontalHeaderLabels(['ID', 'Сорт',
@@ -24,14 +38,93 @@ class Cofeetable(QMainWindow):
                                                     'Вкус', 'Цена(руб.)',
                                                     'Масса(гр.)'])
         for row in range(len(self.data_from_table)):
-                for col in range(len(self.data_from_table[0])):
-                    it = QTableWidgetItem(str(self.data_from_table[row][col]))
-                    self.tableWidget.setItem(row, col, it)
+            for col in range(len(self.data_from_table[0])):
+                it = QTableWidgetItem(str(self.data_from_table[row][col]))
+                self.tableWidget.setItem(row, col, it)
+
+    def open_add_menu(self):
+        self.add_widg = AddCoffee(self)
+        self.add_widg.show()
+
+
+class AddCoffee(QMainWindow):
+    def __init__(self, obj):
+        super().__init__()
+        self.main = obj
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        self.connection = sqlite3.connect('cofee.sqlite')
+        self.cur = self.connection.cursor()
+        self.table_row = ''
+        self.initUI()
+
+    def initUI(self):
+        self.pushButton.clicked.connect(self.ok_btn)
+
+        self.pushButton_2.clicked.connect(self.close)
+
+        self.comboBox.addItems([str(elem[0]) for elem
+                                in self.main.data_from_table])
+        self.comboBox.setCurrentText('')
+        self.comboBox.currentIndexChanged.connect(self.add_data_from_db)
+
+    def ok_btn(self):
+        try:
+            data_list = [int(self.comboBox.currentText()), self.lineEdit.text(),
+                         self.lineEdit_2.text(), self.lineEdit_3.text(),
+                         self.lineEdit_4.text(), int(self.lineEdit_5.text()),
+                         int(self.lineEdit_6.text())]
+            self.table_row = data_list
+            if data_list[0] not in [i[0] for i in self.main.data_from_table]:
+                self.cur.execute(f'''INSERT INTO data
+                            VALUES({data_list[0]}, '{data_list[1]}',
+                                    '{data_list[2]}', '{data_list[3]}',
+                                '{data_list[4]}', {data_list[5]},
+                                                    {data_list[6]})''')
+                self.main.tableWidget.setRowCount(
+                    self.main.tableWidget.rowCount()
+                    + 1)
+                for i in range(7):
+                    self.main.tableWidget.setItem(
+                        self.main.tableWidget.rowCount()
+                        - 1, i,
+                        QTableWidgetItem
+                        (str(self.table_row[i])))
+                self.connection.commit()
+            else:
+                self.cur.execute(f'''UPDATE data  SET Сорт = '{data_list[1]}',
+                        Степень_обжарки = '{data_list[2]}',
+                            Молотый_или_в_зёрнах = '{data_list[3]}',
+                                Вкус = '{data_list[4]}',
+                                    Цена = {data_list[5]},
+                                        Масса_упаковки = {data_list[6]}
+                                                WHERE ID = {data_list[0]}''')
+                self.connection.commit()
+                self.main.show_table()
+
+        except ValueError:
+            msg = QMessageBox(self)
+            msg.setWindowTitle('Ошибка')
+            msg.setText('Некорректный ввод')
+            msg.show()
+            if msg.exec() == QMessageBox.Ok:
+                self.main.open_add_menu()
+
+        self.close()
+
+    def add_data_from_db(self):
+        elems = [i for i in self.main.data_from_table
+                 if str(i[0]) == self.sender().currentText()]
+
+        self.lineEdit.setText(elems[0][1])
+        self.lineEdit_2.setText(elems[0][2])
+        self.lineEdit_3.setText(elems[0][3])
+        self.lineEdit_4.setText(elems[0][4])
+        self.lineEdit_5.setText(str(elems[0][5]))
+        self.lineEdit_6.setText(str(elems[0][6]))
 
 
 if __name__ == '__main__':
     app = QApplication(argv)
-    widg = Cofeetable()
+    widg = Coffeetable()
     widg.show()
     exit(app.exec())
-
